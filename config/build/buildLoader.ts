98823ef66110f49542ec889eeafed5180cddf3b4
Irina946 <Irina.Larina@urfu.me>
2025-07-21 13:33:52 +0500

    Initial

---
+++ config/build/buildLoader.ts
@@ -0,0 +1,36 @@
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import webpack from "webpack";
+import { BuildOptions } from "./types/config";
+
+export function buildLoaders(isDev: boolean): webpack.RuleSetRule[] {
+
+    const cssLoader: webpack.RuleSetRule = {
+        test: /\.s[ac]ss$/i,
+        use: [
+            isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+            {
+                loader: "css-loader",
+                options: {
+                    modules: {
+                        auto: (resPath: string) => Boolean(resPath.includes(".module.")),
+                        localIdentName: isDev
+                            ? "[path][name]__[local]--[hash:base64:5]"
+                            : "[hash:base64:8]",
+                    }
+                }
+            },
+            "sass-loader",
+        ],
+    }
+
+    const typeScriptLoader: webpack.RuleSetRule = {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+    }
+
+    return [
+        typeScriptLoader,
+        cssLoader
+    ]
+}
