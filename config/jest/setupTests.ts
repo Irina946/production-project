c0cc490e79732115a90bb25b4fef3728a8a14865
Irina946 <Irina.Larina@urfu.me>
2025-09-11 22:44:06 +0500

    fix tests

--- config/jest/setupTests.ts
+++ config/jest/setupTests.ts
@@ -4,3 +4,9 @@ import 'regenerator-runtime/runtime';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 (global as any).__IS_DEV__ = true;
+
+if (typeof global.TextEncoder === 'undefined') {
+    const { TextEncoder, TextDecoder } = require('util');
+    global.TextEncoder = TextEncoder;
+    global.TextDecoder = TextDecoder;
+}
