dd006a6ba4e64c53279898a1f77c860951723c91
Irina946 <Irina.Larina@urfu.me>
2025-09-11 22:48:27 +0500

    fix setupTests

--- config/jest/setupTests.ts
+++ config/jest/setupTests.ts
@@ -1,12 +1,12 @@
 import '@testing-library/jest-dom';
 import '@testing-library/jest-dom/jest-globals';
 import 'regenerator-runtime/runtime';
+import { TextEncoder, TextDecoder } from 'util';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 (global as any).__IS_DEV__ = true;
 
 if (typeof global.TextEncoder === 'undefined') {
-    const { TextEncoder, TextDecoder } = require('util');
-    global.TextEncoder = TextEncoder;
-    global.TextDecoder = TextDecoder;
+    global.TextEncoder = TextEncoder as unknown as typeof global.TextEncoder;
+    global.TextDecoder = TextDecoder as unknown as typeof global.TextDecoder;
 }
